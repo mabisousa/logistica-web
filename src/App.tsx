49f3f642e915c2d0d8c21b0a2b9c7ff8c5d04d68
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-07-21 16:44:58 -0300

    Configuração da tela de login e componente input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import GlobalStyle from "./style/global";
+
+import SignIn from "./pages/SignIn";
+
 const App: React.FC = () => {
-  return <h1>Hello World.</h1>;
+  return (
+    <>
+      <SignIn/>
+      <GlobalStyle/>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons'
+import { Container } from './style';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({icon, ...rest}) => (
+    <Container>
+      <input  {...rest}/>
+    </Container>
+);
+
+export default Input;

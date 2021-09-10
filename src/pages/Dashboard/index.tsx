6540fb647e4f70d068b73dbd12195457d155c146
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-09-10 08:18:23 -0300

    Criação da página dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import { Container } from './style'
+
+const Dashboard: React.FC = () => {
+    return (
+    <Container>
+        <h1>dashboard</h1>
+    </Container>
+    )
+}
+
+export default Dashboard;

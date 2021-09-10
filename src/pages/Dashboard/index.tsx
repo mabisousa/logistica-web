4f1d22cf63189a75f0d399988d8a37912039d589
Mabi Sousa <maria_gs_cruz@estudante.sc.senai.br>
2021-09-10 08:53:47 -0300

    Conexão com a api para exibição de dados

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,30 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from './style'
+import api from "../../service/api"
+
+interface Pessoa{
+    nome: string;
+    telefone: string;
+}
 
 const Dashboard: React.FC = () => {
+    const [ pessoas, setPessoa ] = useState<Pessoa[]>([]);
+
+    useEffect(() => {
+        api.get("/pessoas").then((response) => {
+            setPessoa(response.data);
+        })
+    }, [])
+
     return (
     <Container>
         <h1>dashboard</h1>
+    {pessoas.map((pessoa) => (
+        <div>
+            <p>{pessoas.map((pessoa) => pessoa.nome)}</p>
+        </div>
+    ))}
+        
     </Container>
     )
 }

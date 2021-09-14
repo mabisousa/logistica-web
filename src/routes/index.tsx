2723d20c2dd88ff0c47a54628b7d050a7082f433
mabisousa <maria_gs_cruz@estudante.sc.senai.br>
2021-09-14 15:04:05 -0300

    Criação de rota "cadastro"

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import Route from "./Route"
 import SignUp from '../pages/SingUp';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
+import Cadastro from '../pages/Cadastro';
 
 const Routes: React.FC = () => (
     <Switch>
         <Route path="/" exact component={ SignIn }/>
         <Route path="/signup" component={ SignUp }/>
         <Route path="/dashboard" component={ Dashboard } isPrivate/>
+        <Route path="/cadastro" component={Cadastro} isPrivate/>
     </Switch>
 );
 
